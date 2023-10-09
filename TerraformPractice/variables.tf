2fb1fbdecc41d470aaa6a9dbf99b7d9df41f6c93
Jyoti Minocha <jyotiminocha13@gmail.com>
2023-10-09 09:15:34 -0400

    Add files via upload

---
+++ TerraformPractice/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "value"
+  type        = string
+  default     = "West US"
+}
+variable "resource_group_name" {
+  description = "name of rg"
+  type        = string
+}
