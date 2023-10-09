dbb9c6d234334370e6f68bb54127a2b9a027ec3d
Jyoti Minocha <jyotiminocha13@gmail.com>
2023-10-09 09:17:23 -0400

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  description = "value"
-  type        = string
-  default     = "West US"
-}
-variable "resource_group_name" {
-  description = "name of rg"
-  type        = string
-}
